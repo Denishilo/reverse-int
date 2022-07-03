cd2e6195785c25440b989f4b02a4b1999065dd31
Denishilo <denisanishenko@gmail.com>
2022-07-03 16:59:30 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let stringOfNumber = String(Math.abs(n));
+    let reverseOfString = stringOfNumber.split('').reverse().join('');
+    return +reverseOfString;
 }
